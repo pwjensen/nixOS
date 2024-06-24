ac93a5a41208f856aa2fcbd12d7ab7b7418984c4
Paul <jensenmusic@outlook.com>
2024-06-24 17:20:04 -0400

    NVIM

--- modules/home-manager/apps/nvim.nix
+++ modules/home-manager/apps/nvim.nix
@@ -2,12 +2,86 @@
 
 {
   programs.neovim = {
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
     enable = true;
 
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
 
+    extraPackages = with pkgs; [
+      lua-language-server
+      rnix-lsp
 
+      xclip
+      wl-clipboard
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+
+      {
+        plugin = comment-nvim;
+        config = toLua ''require("Comment").setup()'';
+      }
+
+      {
+        plugin = tokyonight-nvim;
+        config = "colorscheme tokyonight";
+      }
+
+      neodev-nvim
+
+      nvim-cmp
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./nvim/plugin/cmp.lua;
+      }
+
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+
+      telescope-fzf-native-nvim
+
+      cmp_luasnip
+      cmp-nvim-lsp
+
+      luasnip
+      friendly-snippets
+
+
+      lualine-nvim
+      nvim-web-devicons
+
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-json
+        ]));
+        config = toLuaFile ./nvim/plugin/treesitter.lua;
+      }
+
+      vim-nix
+    ];
+
+    extraLuaConfig =
+    ''
+      ${builtins.readFile ./nvim/options.lua}
+    '';
   };
+
 }
